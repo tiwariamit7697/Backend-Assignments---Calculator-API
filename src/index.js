41eb4e1657f270ca1f241cb7bcc34fc2c3230a8b
Amit <amitkumartiwari527@gmaial.com>
2020-12-09 00:21:09 +0530

    code changed

--- src/index.js
+++ src/index.js
@@ -1,204 +1,152 @@
-const express = require('express')
-const app = express()
-const bodyParser = require("body-parser");
-const port = 3000
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
+const maxLimit = 1000000;
+const minLimit = -1000000;
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // your code goes here
-app.get("/",(req,res)=>{
-    res.send("Hello world!");
-});
-app.post("/add",(req,res)=>{
-    const body=req.body;
-    const num1=body.num1;
-    const num2=body.num2;
-    if(num1<-1000000 || num2<-1000000)
-    {
-        res.send(
-            {
-                status:"error",
-                message: "Underflow"
-            }
-        );
-    }
-    else if(num1>1000000 || num2>1000000 || sumResult>1000000)
-    {
-        res.send(
-            {
-                status:"error",
-                message: "Overflow"
-            }
-        );
-    }
-    else if(typeof num1=="string" || typeof num2=="string")
-    {
-        res.send(
-            {
-                status:"error",
-                message: "Invalid data types"
-            }
-        );
-    }
-    else
-    {
-        res.send(
-            {
-                status: "success",
-                message: "the sum of given two numbers",
-                sum: num1+num2
-            }
-        );
-    }
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
 });
 
-app.post("/sub",(req,res)=>{
-    const body=req.body;
-    const num1=body.num1;
-    const num2=body.num2;
-    
-    if(num1<-1000000 || num2<-1000000)
-    {
-        res.send(
-            {
-                status:"error",
-                message: "Underflow"
-            }
-        );
-    }
-    else if(num1>1000000 || num2>1000000)
-    {
-        res.send(
-            {
-                status:"error",
-                message: "Overflow"
-            }
-        );
-    }
-    else if(typeof num1=="string" || typeof num2=="string")
-    {
-        res.send(
-            {
-                status:"error",
-                message: "Invalid data types"
-            }
-        );
-    }
-    else
-    {
-        res.send(
-            {
-                status: "success",
-                message: "the difference of given two numbers",
-                difference: num1-num2
-            }
-        );
-    }
+app.post('/add', (req, res) => {
+  const firstNum = req.body.num1;
+  const secondNum = req.body.num2;
+  if (typeof firstNum == 'string' || typeof secondNum == 'string') {
+    res.send({
+      status: 'error',
+      message: 'Invalid data types',
+    });
+  }
+  //   console.log(firstNum + ' ' + secondNum );
+  if (firstNum < minLimit || secondNum < minLimit) {
+    res.send({
+      status: 'error',
+      message: 'Underflow',
+    });
+  } else if (firstNum > maxLimit || secondNum > maxLimit) {
+    res.send({
+      status: 'error',
+      message: 'Overflow',
+    });
+  } else if (firstNum + secondNum > maxLimit) {
+    res.send({
+      status: 'error',
+      message: 'Overflow',
+    });
+  } else {
+    res.send({
+      status: 'success',
+      message: 'the sum of given two numbers',
+      sum: firstNum + secondNum,
+    });
+  }
 });
 
-
-app.post("/multiply",(req,res)=>{
-    const body=req.body;
-    const num1=body.num1;
-    const num2=body.num2;
-    if(num1<-1000000 || num2<-1000000 || (num1*num2) <-1000000)
-    {
-        res.send(
-            {
-                status:"error",
-                message: "Underflow"
-            }
-        );
-    }
-    else if(num1>1000000 || num2>1000000)
-    {
-        res.send(
-            {
-                status:"error",
-                message: "Overflow"
-            }
-        );
-    }
-    else if(typeof num1=="string" || typeof num2=="string")
-    {
-        res.send(
-            {
-                status:"error",
-                message: "Invalid data types"
-            }
-        );
-    }
-    else
-    {
-        res.send(
-            {
-                status: "success",
-                message: "the product of given two numbers",
-                result: num1*num2
-            }
-        );
-    }
+app.post('/sub', (req, res) => {
+  const firstNum = req.body.num1;
+  const secondNum = req.body.num2;
+  if (typeof firstNum == 'string' || typeof secondNum == 'string') {
+    res.send({
+      status: 'error',
+      message: 'Invalid data types',
+    });
+  }
+  if (firstNum < minLimit || secondNum < minLimit) {
+    res.send({
+      status: 'error',
+      message: 'Underflow',
+    });
+  } else if (firstNum > maxLimit || secondNum > maxLimit) {
+    res.send({
+      status: 'error',
+      message: 'Overflow',
+    });
+  } else {
+    res.send({
+      status: 'success',
+      message: 'the difference of given two numbers',
+      difference: firstNum - secondNum,
+    });
+  }
 });
 
-app.post("/divide",(req,res)=>{
-    const body=req.body;
-    const num1=body.num1;
-    const num2=body.num2;
-    if(num2==0)
-    {
-        res.send(
-            {
-                status:"error",
-                message: "Cannot divide by zero"
-            }
-        );
-    }
-    else if(num1<-1000000 || num2<-1000000)
-    {
-        res.send(
-            {
-                status:"error",
-                message: "Underflow"
-            }
-        );
-    }
-    else if(num1>1000000 || num2>1000000)
-    {
-        res.send(
-            {
-                status:"error",
-                message: "Overflow"
-            }
-        );
-    }
-    else if(typeof num1=="string" || typeof num2=="string")
-    {
-        res.send(
-            {
-                status:"error",
-                message: "Invalid data types"
-            }
-        );
-    }
-    
-    else
-    {
-        res.send(
-            {
-                status: "success",
-                message: "the division of given two numbers",
-                result: num1/num2
-            }
-        );
-    }
+app.post('/multiply', (req, res) => {
+  const firstNum = req.body.num1;
+  const secondNum = req.body.num2;
+  if (typeof firstNum == 'string' || typeof secondNum == 'string') {
+    res.send({
+      status: 'error',
+      message: 'Invalid data types',
+    });
+  }
+  if (firstNum > maxLimit || secondNum > maxLimit) {
+    res.send({
+      status: 'error',
+      message: 'Overflow',
+    });
+  } else if (firstNum < minLimit || secondNum < minLimit) {
+    res.send({
+      status: 'error',
+      message: 'Underflow',
+    });
+  } else if (firstNum * secondNum > maxLimit) {
+    res.send({
+      status: 'error',
+      message: 'Overflow',
+    });
+  } else {
+    res.send({
+      status: 'success',
+      message: 'The product of given numbers',
+      result: firstNum * secondNum,
+    });
+  }
 });
 
-app.listen(port);
+app.post('/divide', (req, res) => {
+  const firstNum = req.body.num1;
+  const secondNum = req.body.num2;
+  console.log(typeof firstNum + ' ' + typeof secondNum);
+  if (secondNum === 0) {
+    res.send({
+      status: 'error',
+      message: 'Cannot divide by zero',
+    });
+  } else if (typeof firstNum == 'string' || typeof secondNum == 'string') {
+    res.send({
+      status: 'error',
+      message: 'Invalid data types',
+    });
+  }
+  if (firstNum < minLimit || secondNum < minLimit) {
+    res.send({
+      status: 'error',
+      message: 'Underflow',
+    });
+  } else if (firstNum > maxLimit || secondNum > maxLimit) {
+    res.send({
+      status: 'error',
+      message: 'Overflow',
+    });
+  } else {
+    res.send({
+      status: 'success',
+      message: 'The division of given numbers',
+      result: firstNum / secondNum,
+    });
+  }
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
